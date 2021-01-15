361401d9d8adc2e3bc1b3b40d279f8e2738745d3
whack <aj0964@students.necsd.net>
2021-01-15 16:15:46 +0000

    prob1 & prob 2

--- Prob1.java
+++ Prob1.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 class Prob1 {
   public static void main(String[] args) {
-    
+    Scanner scan = new Scanner(System.in);
+    system.out.print("Enter a sentence: ");
+    String first = scan.NextString();
+    System.out.print(firstWord.indexOf(" "));
   }
 }
--- Prob2.java
+++ Prob2.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 class Prob2 {
   public static void main(String[] args) {
-    
+   Scanner scan = new Scanner(System.in); 
+   System.out.print("Enter a String: ");
+   String word = Scan.nextLine();
+   System.out.println(word.substring());
+
   }
 }
