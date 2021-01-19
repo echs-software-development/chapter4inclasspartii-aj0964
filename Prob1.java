dbb5ca5584ebd5c6bc54d90a4b0a3cd1262b2f5c
whack <aj0964@students.necsd.net>
2021-01-19 15:18:40 +0000

    W.I.P

--- Prob1.java
+++ Prob1.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 class Prob1 {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    system.out.print("Enter a sentence: ");
-    String first = scan.NextString();
-    System.out.print(firstWord.indexOf(" "));
+    System.out.print("Enter a sentence: ");
+    String firstWord = scan.nextLine();
+    System.out.print(firstWord.indexOf(0,1));
+    System.out.println(word.length(-1));
   }
 }
--- Prob3.java
+++ Prob3.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 class Prob3 {
   public static void main(String[] args) {
-    
+   Scanner scan = new Scanner(System.in);
+   System.out.print("Enter a word: ");
+   String word =  scan.nextLine();
+   word = word.toLowerCase();
+   Strung word2
   }
 }
