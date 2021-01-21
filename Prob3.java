9cfb441b5b38a0be31bbf68ae3da3d4eeb6d22c9
whack <aj0964@students.necsd.net>
2021-01-21 16:09:49 +0000

    prob 3 fix

--- Prob3.java
+++ Prob3.java
@@ -4,8 +4,13 @@ class Prob3 {
   public static void main(String[] args) {
    Scanner scan = new Scanner(System.in);
    System.out.print("Enter a word: ");
+
    String word =  scan.nextLine();
    word = word.toLowerCase();
-   String word
+
+   System.out.println("Enter another word: ");
+   String word2 = scan.nextLine();
+   word2 = word2.toLowerCase();
+   System.out.print(word.compareTo(word2));
   }
 }
